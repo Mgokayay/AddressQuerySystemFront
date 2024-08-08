3c229c83926f2ef621c571cc2833deb5f05f984c
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-08 16:04:07 +0300

    home page created

--- addressfront/src/pages/HomePage.js
+++ addressfront/src/pages/HomePage.js
@@ -3,10 +3,23 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <Link to="add-address">Press the button to add a address</Link>
-      <Link to="show-address">Press the button to show address</Link>
+    <div className="flex flex-col h-[100vh] items-center justify-center gap-8">
+      <h1 className="font-bold text-3xl text-blue-900">Welcome</h1>
+      <h1 className="text-base text-blue-900">
+        You can add a new address or list your addresses
+      </h1>
+      <Link
+        to="add-address"
+        className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200"
+      >
+        <h className="text-white">Add New Address</h>
+      </Link>
+      <Link
+        to="show-address"
+        className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200"
+      >
+        <h className="text-white">Show Addresses</h>
+      </Link>
     </div>
   );
 };
