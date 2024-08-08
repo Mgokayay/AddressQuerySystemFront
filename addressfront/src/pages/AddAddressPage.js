46cb35f78d47cffd6727328c76b4b1c5271a8192
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-08 16:03:53 +0300

    add address page created

--- addressfront/src/pages/AddAddressPage.js
+++ addressfront/src/pages/AddAddressPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AddAddressPage = () => {
   const [province, setProvince] = useState("");
   const [district, setDistrict] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
 
   const handleProvinceChange = (event) => {
     setProvince(event.target.value);
@@ -22,37 +24,67 @@ const AddAddressPage = () => {
         "http://localhost:9001/mavidevv/address",
         data
       );
+      setStatusMessage("Province and District successfully saved!");
       console.log("Province Response:", response.data);
     } catch (error) {
+      setStatusMessage("There was an error posting the data!");
       console.error("There was an error posting the data!", error);
     }
   };
 
   return (
-    <div>
-      <h1>Add Province and District</h1>
+    <div className="flex flex-col h-[100vh] items-center justify-center gap-8">
+      <h1 className="font-bold text-2xl text-blue-900">
+        Add Province and District
+      </h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Province:
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <h className="text-orange-500 font-medium">Province</h>
             <input
               type="text"
               value={province}
               onChange={handleProvinceChange}
+              className="ml-2 border-2 rounded"
             />
-          </label>
-        </div>
-        <div>
-          <label>
-            District:
+          </div>
+          <div className="flex justify-between">
+            <h className="text-orange-500 font-medium">District</h>
             <input
               type="text"
               value={district}
               onChange={handleDistrictChange}
+              className="ml-2 border-2 rounded"
             />
-          </label>
+          </div>
+          <div className="flex justify-center mt-2">
+            <button
+              type="submit"
+              className="border-2 p-[4px] border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200 w-[30%]"
+            >
+              <h className="text-white">Save</h>
+            </button>
+          </div>
+          {statusMessage && (
+            <div className="flex justify-center mt-2 text-green-500">
+              {statusMessage}
+            </div>
+          )}
+          <div className="flex justify-center mt-2 gap-2">
+            <Link
+              to="/"
+              className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200 "
+            >
+              <h className="text-white">Go to HomePage</h>
+            </Link>
+            <Link
+              to="/show-address"
+              className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200"
+            >
+              <h className="text-white">Go to show address</h>
+            </Link>
+          </div>
         </div>
-        <button type="submit">Save</button>
       </form>
     </div>
   );
