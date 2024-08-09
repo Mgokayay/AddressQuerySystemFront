7f46fb344d39de64ccd2230799b2da5b6df87084
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-09 17:37:41 +0300

    new feature added

--- addressfront/src/pages/ShowAddressPage.js
+++ addressfront/src/pages/ShowAddressPage.js
@@ -31,30 +31,42 @@ const ShowAddressPage = () => {
 
   return (
     <div className="flex flex-col h-[100vh] items-center justify-center gap-8">
-      <h2 className="font-bold text-2xl text-blue-900 ">Adresses</h2>
+      <h2 className="font-bold text-2xl text-blue-900">Adresses</h2>
       <div className="border-2 border-blue-900 rounded-md w-[50%] h-[50%] overflow-auto">
-        {data?.map((address, index) => (
-          <div key={index} className="flex py-3 border-b-2 ">
-            <div className="border-r-2 w-[40%] text-center">
-              {address.province}
-            </div>
-            <div className="w-[40%] text-center">{address.district}</div>
-            <button
-              onClick={() => handleDelete(address.id)}
-              className="border-r-2 w-[20%] text-red-500"
-            >
-              Delete
-            </button>
+        {data.length === 0 ? (
+          <div className="text-center py-4 text-gray-500">
+            There is no data to show
           </div>
-        ))}
+        ) : (
+          data?.map((address, index) => (
+            <div key={index} className="flex py-3 border-b-2">
+              <div className="border-r-2 w-[40%] text-center">
+                {address.province}
+              </div>
+              <div className="w-[40%] text-center">{address.district}</div>
+              <button
+                onClick={() => handleDelete(address.id)}
+                className="border-r-2 w-[20%] text-red-500"
+              >
+                Delete
+              </button>
+            </div>
+          ))
+        )}
       </div>
 
-      <div className="flex justify-center mt-2">
+      <div className="flex justify-center mt-2 gap-4">
         <Link
           to="/add-address"
           className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200"
         >
-          <h className="text-white">Add new address</h>
+          <span className="text-white">Add new address</span>
+        </Link>
+        <Link
+          to="/"
+          className="border-2 p-2 border-blue-900 rounded-lg bg-orange-500 hover:scale-105 transition duration-200"
+        >
+          <span className="text-white">Go to Home Page</span>
         </Link>
       </div>
     </div>
